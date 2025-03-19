f4cb1c4b183dcce67825f8de8460014f9c01a346
NichapaJongKmutt <nichapa.jong@mail.kmutt.ac.th>
2025-03-19 12:35:12 +0700

    Delete csc105-HW03-67130500809-Nichapa_Jongsakulsiri/profile directory

--- csc105-HW03-67130500809-Nichapa_Jongsakulsiri/profile/src/ContactBtn.jsx
+++
@@ -1,8 +0,0 @@
-function ContactBtn(){
-    return(
-        <div className="contactbtn bg-blue-400 px-3 py-2 rounded-3xl text-white shadow-md">
-            Contact
-        </div>
-    )
-}
-export default ContactBtn;
